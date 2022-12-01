2f49872554888c3a1b799204b7f01679f50bbe16
ChanHeinKyaw <scm.chanheinkyaw@gmail.com>
2022-12-01 15:56:46 +0630

    layouts

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <header>
+        <nav>
+          <NuxtLink to="/">Lufy Shop</NuxtLink>
+          <ul>
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+            <li><NuxtLink to="/products">Products</NuxtLink></li>
+          </ul>
+          <a href="/about">normal link -- about page</a>
+        </nav>
+      </header>
+
+      <!-- output the page content -->
+      <div>
+        <slot/>
+      </div>
+    </div>
+  </template>
+  
+  <script setup></script>
+  <style scoped>
+  .router-link-exact-active{
+    color: #12b488;
+  }
+
+  </style>
+  
