9a2682d6f45c662e188d2edce5fcdd510c85a7e0
ChanHeinKyaw <scm.chanheinkyaw@gmail.com>
2022-12-29 09:33:52 +0630

    error-pages

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -12,6 +12,10 @@
     const { id } = useRoute().params
     const url = "https://fakestoreapi.com/products/" + id;
     const {data: product} =await useFetch(url, {key: id})
+
+    if(!product.value){
+        throw createError({statusCode: 404, statusMessage: 'Product not found', fatal: true});
+    }
 </script>
 
 <style scoped>
